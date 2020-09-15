cb789e58ca2c226ba656d60c54e7c2e0ec756fdf
Egor-arkharov <egorarkharov@gmail.com>
2020-09-15 22:00:25 +0300

    Стили, пиксель перфект

---
+++ source/js/scroll.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+
+  let prevScrollpos = window.pageYOffset;
+  let mainImg = document.querySelector('.nav__img');
+
+  // window.addEventListener('scroll', function() {
+  //   let currentScrollPos = window.pageYOffset;
+
+  //   if (prevScrollpos > currentScrollPos) {
+  //     mainImg.style.marginTop = "-111px";
+  //   } else {
+  //     mainImg.style.marginTop = "-1792px";
+  //   }
+  //   prevScrollpos = currentScrollPos;
+
+  // });
+})();
